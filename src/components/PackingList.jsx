d2248a7fdcd593ed1b2a4d09204d578e16e9130c
eminentishere <eminentishere@outlook.com>
2023-07-12 07:02:01 +0500

    added list

---
+++ src/components/PackingList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+]
+
+const PackingList = () => {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  )
+}
+
+export default PackingList
