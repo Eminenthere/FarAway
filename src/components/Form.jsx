34abbfa170fa882950990ef8621944859c7a562a
eminentishere <eminentishere@outlook.com>
2023-07-14 08:24:09 +0500

    updating form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+  const [description, setDescription] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (!description) return
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem)
+    setDescription('')
+    setQuantity(1)
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item ...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   )
 }
 
